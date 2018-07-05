fb426b7b829c41b8d5e6cc577ab5a9a0d33b8bea
Naveen Jangid <jangidnaveen39@gmail.com>
2018-07-06 01:05:03 +0530

    Initial Commit

--- SimpleContactManager/MainPage.xaml.cs
+++ SimpleContactManager/MainPage.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace SimpleContactManager
@@ -22,9 +11,28 @@ namespace SimpleContactManager
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private List<Icon> Icons;
+        private ObservableCollection<Contact> Contacts;
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            Icons = new List<Icon>();
+            Icons.Add(new Icon { IconSource = "Assets/female-01.png" });
+            Icons.Add(new Icon { IconSource = "Assets/female-02.png" });
+            Icons.Add(new Icon { IconSource = "Assets/female-03.png" });
+            Icons.Add(new Icon { IconSource = "Assets/male-01.png" });
+            Icons.Add(new Icon { IconSource = "Assets/male-02.png" });
+            Icons.Add(new Icon { IconSource = "Assets/male-03.png" });
+
+            Contacts = new ObservableCollection<Contact>();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string avatar = ((Icon)AvatarComboBox.SelectedValue).IconSource;
+            Contacts.Add(new Contact { FirstName = FirstNameBox.Text, Avatar = avatar, LastName=LastNameBox.Text});
         }
     }
 }
